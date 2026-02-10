c8c5c0373e7193e5d5438d89b184d5f1ca4e6dac
Dineth Hirusha <dineth.hirusha.pro@gmail.com>
2026-02-11 00:26:05 +0530

    Add files via upload

---
+++ app/main/java/com/example/meloch/data/model/User.kt
@@ -0,0 +1,10 @@
+package com.example.meloch.data.model
+
+/**
+ * Represents a user in the application
+ */
+data class User(
+    val username: String,
+    val email: String,
+    val password: String
+)
